2798bd8807b9c1c48d6f1b850f42a73c45f60864
RM25BAM <nmpiedrabuena2003@gmail.com>
2024-10-10 00:18:21 -0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Card from './components/Card';
+import Card from './components/card';
 import { AnimatedBackground } from 'animated-backgrounds';
 
 
